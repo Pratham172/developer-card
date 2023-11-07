99f84cce27f138a992389e1dfe88ebbe2359e3b3
Pratham <108515844+Pratham172@users.noreply.github.com>
2023-11-07 21:35:22 +0530

    Update ProfilePhoto.js

--- src/ProfilePhoto.js
+++ src/ProfilePhoto.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import profile from ("./images/profile.jpg");
 
 function ProfilePhoto() {
     // importing profile image from google drive.
     // image file ID from your Google Drive.
-    const imageFileId = '1SdFvm_05jAlTTDlVzKZJLiDGOhQ0yzaI';
+    //const imageFileId = '1SdFvm_05jAlTTDlVzKZJLiDGOhQ0yzaI';
 
     // Construct the URL using the file ID.
-    const imageUrl = `https://drive.google.com/uc?id=${imageFileId}`;
+   // const imageUrl = `https://drive.google.com/uc?id=${imageFileId}`;
 
     return (
 
-        <img className='profile-img' src={imageUrl} loading="lazy" alt="profile" />
+        <img className='profile-img' src={profile} loading="lazy" alt="profile" />
     );
 }
 
